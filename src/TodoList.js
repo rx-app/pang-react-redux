d102ba8b8ac016309558bf48d9ecde07c039ad38
rx-app <rxapp@yandex.com>
2019-11-15 19:18:41 +0800

    改为UI组件（class to function）

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,30 +1,25 @@
 import React, { Component } from 'react';
-import store from './store'
+// import store from './store'
 import {connect} from 'react-redux'
 
-class TodoList extends Component {
-    constructor(props){
-        super(props)
-        this.state = store.getState()
-    }
-    render() { 
-        let {inputValue,inputChange,addItem,list} = this.props
-        return (
+const TodoList = (props) => {
+    
+    let {inputValue,inputChange,addItem,list} = props
+    return (
+        <div>
             <div>
-                <div>
-                    <input value={inputValue} onChange={inputChange} />
-                    <button onClick={addItem}>提交</button>
-                </div>
-                <ul>
-                    {
-                      list.map((item,index)=>{
-                        return <li key={index}>{item}</li>
-                      })
-                    }
-                </ul>
+                <input value={inputValue} onChange={inputChange} />
+                <button onClick={addItem}>提交</button>
             </div>
-            );
-    }
+            <ul>
+                {
+                    list.map((item,index)=>{
+                    return <li key={index}>{item}</li>
+                    })
+                }
+            </ul>
+        </div>
+        );
 }
 const stateToProps = (state)=>{
     return {
